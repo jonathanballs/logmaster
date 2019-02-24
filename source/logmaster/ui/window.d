36ac0a82570b07dada38f67c964cbf0405461364
Jonathan Balls <jonathanballs@protonmail.com>
2019-02-24 13:24:50 +0000

    Override default addTickCallback functions until fixes arrive in Gtkd release

--- source/logmaster/ui/window.d
+++ source/logmaster/ui/window.d
@@ -329,4 +329,28 @@ class LogmasterWindow : MainWindow {
         this.notebook.setCurrentPage(pageNum);
         this.notebook.show();
     }
+
+    override void addTickCallback(bool delegate(Widget, FrameClock) callback)
+    {
+        tickCallbackListeners ~= callback;
+        static bool connected;
+
+        if ( connected )
+        {
+            return;
+        }
+
+        super.addTickCallback(cast(GtkTickCallback)&tickCallback, cast(void*)this, null);
+        connected = true;
+    }
+
+    extern(C) static int tickCallback(GtkWidget* widgetStruct, GdkFrameClock* frameClock, Widget _widget)
+    {
+        import std.algorithm.iteration : filter;
+        import std.array : array;
+        _widget.tickCallbackListeners = _widget.tickCallbackListeners.filter!((dlg) {
+            return dlg(_widget, new FrameClock(frameClock));
+        }).array();
+        return !!_widget.tickCallbackListeners.length;
+    }
 }
